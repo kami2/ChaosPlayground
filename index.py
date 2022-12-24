0e79b73c36b2343969f8fc79bb3050589ccb3f3a
kami2 <kami.krasowski.uk@gmail.com>
2022-12-24 09:39:33 +0000

    vercel fix

--- index.py
+++ index.py
@@ -3,20 +3,12 @@ from apps.test_import import TestImport
 
 app = Flask(__name__)
 
-# If file name is app.py or main.py then project will not start on vercel
-
 
 @app.route("/")
 def main():
     enter_text = "Do something..."
     results = render_template("index.html", enter_text=enter_text)
     return results
-
-
-@app.route("/test")
-def test_area():
-    TestImport("Did i make mistake?")
-    return "test import"
 
 
 if __name__ == '__main__':
