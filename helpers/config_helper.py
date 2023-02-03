a815886ce0b5e8f88f7256242bb7ed839b2af35f
kami2 <kami.krasowski.uk@gmail.com>
2023-02-03 20:04:33 +0000

    updated silly config helper

--- helpers/config_helper.py
+++ helpers/config_helper.py
@@ -11,14 +11,15 @@ class ConfigHelper:
 
     def __init__(self, config_file: str = None):
         self.config = load_dotenv(DEFAULT_CONFIG)
-        if config_file is not None:
-            logging.info(f"Get config from: {config_file}")
-            self.config = load_dotenv(config_file)
+        self.config_file = config_file
+
+        if self.config_file is not None:
+            logging.info(f"Get config from: {self.config_file}")
+            self.config = load_dotenv(self.config_file)
 
     def get_config(self, key: str):
-        if self.config:
+        if self.config_file:
             logging.info(f"Get variable {key} from config: {CONFIG_NAME}")
-            return os.environ.get(key)
         else:
             logging.info(f"Get environment variable {key}")
-            return os.environ.get(key)
+        return os.environ.get(key)
