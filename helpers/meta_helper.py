d68f5fff097668d01b5da2e9365f582719c88549
kami2 <kami.krasowski.uk@gmail.com>
2023-09-18 21:05:43 +0100

    Initial MetaHelper commit, too tired today

---
+++ helpers/meta_helper.py
@@ -0,0 +1,16 @@
+from helpers.config_helper import ConfigHelper
+import requests
+
+
+class MetaHelper:
+    BASE_URL = "https://graph.instagram.com/v18.0/"
+
+    def __init__(self):
+        pass
+
+    # setup this shit is a nightmare
+
+
+if __name__ == '__main__':
+    meta = MetaHelper()
+
