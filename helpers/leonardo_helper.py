6ef6d10ac9bdcc853d6dcdcd8a37e31d2dcc06b0
kami2 <kami.krasowski.uk@gmail.com>
2023-10-07 18:06:41 +0100

    Updated leonardo_helper

--- helpers/leonardo_helper.py
+++ helpers/leonardo_helper.py
@@ -8,20 +8,37 @@ class LeonardoHelper:
         self.headers = self.get_headers()
 
     def get_headers(self):
-        return {"Authorization": f"Bearer {self.config.get_config('LEONARDO_ACCESS_TOKEN')}"}
+        return {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "Authorization": f"Bearer {self.config.get_config('LEONARDO_ACCESS_TOKEN')}"
+        }
 
     def get_prompt(self):
         # Prompt generator? Not sure if I want use leonardo prompt generator or chatgpt
         pass
 
     def generate_image(self, prompt: str):
-        # Build params and send request
-        params = {
-            "prompt": prompt
+        # https://docs.leonardo.ai/reference/creategeneration
+        generation_id = None
+        url = "https://cloud.leonardo.ai/api/rest/v1/generations"
+        payload = {
+            "height": 768,
+            "modelId": "6bef9f1b-29cb-40c7-b9df-32b51c1f67d3",
+            "prompt": prompt,
+            "num_images": 1,
+            "width": 512,
+            "alchemy": True,
+            "nsfw": False
         }
-        response = requests.get("url", params=params, headers=self.get_headers())
-        return {"Status": response.status_code}
+        response = requests.post(url=url, json=payload, headers=self.get_headers())
+        if response.status_code == 200:
+            generation_id = response.json()['sdGenerationJob']['generationId']
+        return generation_id
 
-    def check_status(self):
-        # maybe I should check if image is ready or still processing
-        pass
+    def get_generation(self, generation_id: str):
+        # https://docs.leonardo.ai/reference/getgenerationbyid
+        url = f"https://cloud.leonardo.ai/api/rest/v1/generations/{generation_id}"
+        response = requests.get(url=url, headers=self.get_headers())
+        # TODO need to do extra stuff here
+        return {"Status": response.status_code}
