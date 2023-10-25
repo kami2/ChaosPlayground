861830be197b6040e3af9e6aa9f77d9c666affb8
kami2 <kami.krasowski.uk@gmail.com>
2023-10-25 18:02:38 +0100

    Cleaning

--- helpers/meta_helper.py
+++ helpers/meta_helper.py
@@ -7,17 +7,14 @@ class InstaHelper:
 
         self.user_id = None
         self.config = ConfigHelper()
-        self.client_secret = self.config.get_config("INSTAGRAM_APP_SECRET")
-        self.client_id = self.config.get_config("INSTAGRAM_APP_ID")
         self.access_token = self.config.get_config("INSTAGRAM_ACCESS_TOKEN")
 
         self.url = f"https://graph.facebook.com/v18.0/{self.config.get_config('INSTAGRAM_ACCOUNT_ID')}"
 
-    # probably I do not need this, will find out tomorrow
-    def get_token(self):
+    def get_long_lived_access_token(self):
         params = {"grant_type": "fb_exchange_token",
-                  "client_id": self.client_id,
-                  "client_secret": self.client_secret,
+                  "client_id":  self.config.get_config("INSTAGRAM_APP_ID"),
+                  "client_secret": self.config.get_config("INSTAGRAM_APP_SECRET"),
                   "fb_exchange_token": self.access_token}
         response = requests.get("https://graph.facebook.com/v18.0/oauth/access_token", params=params)
         return response.json()
@@ -44,5 +41,5 @@ if __name__ == '__main__':
     meta = InstaHelper()
     image = "https://cdn.leonardo.ai/users/e3428632-6606-4fcd-a690-a32e0f9437d1/generations/51f24f72-3511-423f-8238-0c3ce205a88a/Isometric_Scifi_Buildings_Step_into_a_world_of_vibrant_colors_0.jpg"
     print(meta.post_image_on_instagram(image, "Caption here #whatamdoinghere"))
-    # print(meta.get_token())
+
 
